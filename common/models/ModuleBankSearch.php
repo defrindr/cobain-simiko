8aa103702e9fda34f9c4b2babf779cf4e70fb75b
defrindr <defrindr@gmail.com>
2019-02-08 04:57:16 +0700

    adding module bank

---
+++ common/models/ModuleBankSearch.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace common\models;
+
+use Yii;
+use yii\base\Model;
+use yii\data\ActiveDataProvider;
+use common\models\ModuleBank;
+
+/**
+ * common\models\ModuleBankSearch represents the model behind the search form about `common\models\ModuleBank`.
+ */
+ class ModuleBankSearch extends ModuleBank
+{
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['id', 'created_by', 'created_at', 'updated_by', 'updated_at', 'deleted_by', 'lock'], 'integer'],
+            [['no_rekening', 'nama_bank', 'atas_nama', 'gambar', 'deleted_at'], 'safe'],
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function scenarios()
+    {
+        // bypass scenarios() implementation in the parent class
+        return Model::scenarios();
+    }
+
+    /**
+     * Creates data provider instance with search query applied
+     *
+     * @param array $params
+     *
+     * @return ActiveDataProvider
+     */
+    public function search($params)
+    {
+        $query = ModuleBank::find();
+
+        $dataProvider = new ActiveDataProvider([
+            'query' => $query,
+        ]);
+
+        $this->load($params);
+
+        if (!$this->validate()) {
+            // uncomment the following line if you do not want to return any records when validation fails
+            // $query->where('0=1');
+            return $dataProvider;
+        }
+
+        $query->andFilterWhere([
+            'id' => $this->id,
+            'created_by' => $this->created_by,
+            'created_at' => $this->created_at,
+            'updated_by' => $this->updated_by,
+            'updated_at' => $this->updated_at,
+            'deleted_by' => $this->deleted_by,
+            'deleted_at' => $this->deleted_at,
+            'lock' => $this->lock,
+        ]);
+
+        $query->andFilterWhere(['like', 'no_rekening', $this->no_rekening])
+            ->andFilterWhere(['like', 'nama_bank', $this->nama_bank])
+            ->andFilterWhere(['like', 'atas_nama', $this->atas_nama])
+            ->andFilterWhere(['like', 'gambar', $this->gambar]);
+
+        return $dataProvider;
+    }
+}
