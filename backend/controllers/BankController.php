31915e36a93614db8e6f2f58193a3d551f4bccc3
defrindr <defrindr@gmail.com>
2019-02-08 04:54:45 +0700

    adding module bank

---
+++ backend/controllers/BankController.php
@@ -0,0 +1,156 @@
+<?php
+
+namespace backend\controllers;
+
+use Yii;
+use common\models\ModuleBank;
+use common\models\ModuleBankSearch;
+use yii\web\Controller;
+use yii\web\NotFoundHttpException;
+use yii\filters\VerbFilter;
+
+/**
+ * BankController implements the CRUD actions for ModuleBank model.
+ */
+class BankController extends Controller
+{
+    public function behaviors()
+    {
+        return [
+            'verbs' => [
+                'class' => VerbFilter::className(),
+                'actions' => [
+                    'delete' => ['post'],
+                ],
+            ],
+        ];
+    }
+
+    /**
+     * Lists all ModuleBank models.
+     * @return mixed
+     */
+    public function actionIndex()
+    {
+        $searchModel = new ModuleBankSearch();
+        $dataProvider = $searchModel->search(Yii::$app->request->queryParams);
+
+        return $this->render('index', [
+            'searchModel' => $searchModel,
+            'dataProvider' => $dataProvider,
+        ]);
+    }
+
+    /**
+     * Displays a single ModuleBank model.
+     * @param integer $id
+     * @return mixed
+     */
+    public function actionView($id)
+    {
+        $model = $this->findModel($id);
+        $providerModuleSpp = new \yii\data\ArrayDataProvider([
+            'allModels' => $model->moduleSpps,
+        ]);
+        return $this->render('view', [
+            'model' => $this->findModel($id),
+            'providerModuleSpp' => $providerModuleSpp,
+        ]);
+    }
+
+    /**
+     * Creates a new ModuleBank model.
+     * If creation is successful, the browser will be redirected to the 'view' page.
+     * @return mixed
+     */
+    public function actionCreate()
+    {
+        Yii:$app->user->can('Admin'){
+            $model = new ModuleBank();
+
+            if ($model->loadAll(Yii::$app->request->post()) && $model->saveAll()) {
+                return $this->redirect(['view', 'id' => $model->id]);
+            } else {
+                return $this->render('create', [
+                    'model' => $model,
+                ]);
+            }
+            
+        }
+    }
+
+    /**
+     * Updates an existing ModuleBank model.
+     * If update is successful, the browser will be redirected to the 'view' page.
+     * @param integer $id
+     * @return mixed
+     */
+    public function actionUpdate($id)
+    {
+        Yii:$app->user->can('Admin'){
+            
+            $model = $this->findModel($id);
+
+            if ($model->loadAll(Yii::$app->request->post()) && $model->saveAll()) {
+                return $this->redirect(['view', 'id' => $model->id]);
+            } else {
+                return $this->render('update', [
+                    'model' => $model,
+                ]);
+            }
+        }
+    }
+
+    /**
+     * Deletes an existing ModuleBank model.
+     * If deletion is successful, the browser will be redirected to the 'index' page.
+     * @param integer $id
+     * @return mixed
+     */
+    public function actionDelete($id)
+    {
+        Yii:$app->user->can('Admin'){
+        $this->findModel($id)->deleteWithRelated();
+
+        return $this->redirect(['index']);
+            
+        }
+    }
+
+    
+    /**
+     * Finds the ModuleBank model based on its primary key value.
+     * If the model is not found, a 404 HTTP exception will be thrown.
+     * @param integer $id
+     * @return ModuleBank the loaded model
+     * @throws NotFoundHttpException if the model cannot be found
+     */
+    protected function findModel($id)
+    {
+        if (($model = ModuleBank::findOne($id)) !== null) {
+            return $model;
+        } else {
+            throw new NotFoundHttpException('The requested page does not exist.');
+        }
+    }
+    
+    /**
+    * Action to load a tabular form grid
+    * for ModuleSpp
+    * @author Yohanes Candrajaya <moo.tensai@gmail.com>
+    * @author Jiwantoro Ndaru <jiwanndaru@gmail.com>
+    *
+    * @return mixed
+    */
+    public function actionAddModuleSpp()
+    {
+        if (Yii::$app->request->isAjax) {
+            $row = Yii::$app->request->post('ModuleSpp');
+            if((Yii::$app->request->post('isNewRecord') && Yii::$app->request->post('_action') == 'load' && empty($row)) || Yii::$app->request->post('_action') == 'add')
+                $row[] = [];
+            return $this->renderAjax('_formModuleSpp', ['row' => $row]);
+        } else {
+            throw new NotFoundHttpException('The requested page does not exist.');
+        }
+    }
+}
